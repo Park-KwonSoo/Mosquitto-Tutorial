361353f73424368fb1f8a49aacfa1679b51500ff
Park-KwonSoo <redbean096@naver.com>
2021-03-29 22:06:08 +0900

    feat. index.js : Linux Mosquitto Boker <connect> Node JS Client

---
+++ index.js
@@ -0,0 +1,12 @@
+const mqtt = require('mqtt');
+const options = {
+    host : '192.168.0.20',
+    port : 1883,
+};
+const client = mqtt.connect(options);
+
+client.subscribe('test');
+
+client.on('message', (topic, message) => {
+    console.log('토픽 : ', topic, '메시지 : ', message.toString());
+});
